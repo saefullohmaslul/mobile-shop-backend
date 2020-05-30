5a1e9f008c288f578b2d981cf5668efb1aba3d00
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-05-30 10:40:45 +0700

    add jwt sign method

---
+++ src/apps/libraries/token/jwt_library.go
@@ -0,0 +1,43 @@
+package token
+
+import (
+	"io/ioutil"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// Claims is settings jwt
+type Claims struct {
+	Payload interface{} `json:"payload"`
+	jwt.StandardClaims
+}
+
+// SignJWT is method to sign jwt with asymetric
+func SignJWT(payload interface{}) (string, error) {
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &Claims{
+		Payload: payload,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	privateKey, err := ioutil.ReadFile("config/key/private.key")
+	if err != nil {
+		return "", err
+	}
+
+	signKey, err := jwt.ParseRSAPrivateKeyFromPEM(privateKey)
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	tokenString, err := token.SignedString(signKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}

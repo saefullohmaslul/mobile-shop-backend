fb77bbe2e7831644ef772020a9058d50b977c21e
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-05-30 23:00:58 +0700

    change type of entity

--- src/db/entity/user_entity.go
+++ src/db/entity/user_entity.go
@@ -3,8 +3,8 @@ package entity
 // User is struct for user entity
 type User struct {
 	Base
-	Username *string `gorm:"type:varchar(50);unique_index" json:"username"`
-	Name     *string `json:"name" faker:"name"`
-	Email    *string `gorm:"type:varchar(100);unique_index" json:"email" faker:"email"`
-	Password *string `gorm:"type:varchar(255)" json:"password" faker:"password"`
+	Username string  `gorm:"type:varchar(50);unique_index" json:"username"`
+	Name     string  `json:"name" faker:"name"`
+	Email    *string `gorm:"type:varchar(100);unique" json:"email" faker:"email"`
+	Password string  `gorm:"type:varchar(255)" json:"password" faker:"password"`
 }
